2ed035cabb80cfc595a199d269d9622971d410e0
andreasdan <andreas.petersen01@gmail.com>
2019-03-21 11:08:15 +0100

    refactor

--- src/com/kea/Controller/SortementController.java
+++ src/com/kea/Controller/SortementController.java
@@ -2,26 +2,18 @@ package com.kea.Controller;
 
 import com.kea.Model.*;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SortementController {
 
+    private DatabaseController dbController;
 	private List<Route> routes;
-	private String jdbcConnectString;
-	private String sqlUsername;
-	private String sqlPassword;
 
 	public SortementController()
 	{
-		jdbcConnectString = "jdbc:mysql://localhost:3306/maschen";
-		sqlUsername = "autosortbot";
-		sqlPassword = "";
-		routes = getRoutesFromDatabase();
+	    dbController = new DatabaseController();
+		routes = dbController.getRoutesFromDatabase();
 	}
 
 	public List<Route> getRoutes() {
@@ -68,69 +60,6 @@ public class SortementController {
 		}
 
 		return trains;
-	}
-
-	private List<Route> getRoutesFromDatabase()
-	{
-		List<Route> routes = new ArrayList<>();
-
-		Route routeOne = new Route();
-		Route routeTwo = new Route();
-		Route routeThree = new Route();
-		Route routeFour = new Route();
-		Route routeFive = new Route();
-
-		try
-		{
-			String query = "SELECT routeId, StopId, StationsId FROM routes";
-
-			Connection connection = DriverManager.getConnection(jdbcConnectString, sqlUsername, sqlPassword);
-			Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-			ResultSet resSet = statement.executeQuery(query);
-
-			while (resSet.next())
-			{
-				int routeId = resSet.getInt("RouteId");
-				int stopId = resSet.getInt("StopId");
-				int stationsId = resSet.getInt("StationsId");
-				Station station = Station.values()[stationsId + 1];
-
-				switch (routeId)
-				{
-					case 1:
-						routeOne.addStation(stopId, station);
-						break;
-					case 2:
-						routeTwo.addStation(stopId, station);
-						break;
-					case 3:
-						routeThree.addStation(stopId, station);
-						break;
-					case 4:
-						routeFour.addStation(stopId, station);
-						break;
-					case 5:
-						routeFive.addStation(stopId, station);
-						break;
-				}
-			}
-
-			if (resSet!=null) { resSet.close(); }
-			if (statement!=null) { statement.close(); }
-			if (connection!=null) { connection.close(); }
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-
-		routes.add(routeOne);
-		routes.add(routeTwo);
-		routes.add(routeThree);
-		routes.add(routeFour);
-		routes.add(routeFive);
-
-		return routes;
 	}
 
 }
